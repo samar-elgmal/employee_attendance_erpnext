5626233b7f8d56c5d0dd05eabe47ec4bbfcb78c0
samar <samar.elgmal@gmail.com>
2026-04-04 19:02:19 +0000

    fix: test cleanup with try/finally; add HR User read permission to Flexible Hours Settings

--- employee_custom_attendance/employee_custom_attendance/doctype/flexible_hours_settings/test_flexible_hours_settings.py
+++ employee_custom_attendance/employee_custom_attendance/doctype/flexible_hours_settings/test_flexible_hours_settings.py
@@ -4,18 +4,19 @@ from frappe.tests.utils import FrappeTestCase
 
 
 class TestFlexibleHoursSettings(FrappeTestCase):
-    def test_defaults_are_correct(self):
-        settings = frappe.get_single("Flexible Hours Settings")
-        self.assertEqual(settings.weekly_target_hours, 54)
-        self.assertEqual(settings.daily_min_hours, 7)
-        self.assertEqual(settings.daily_max_hours, 11)
+	def test_defaults_are_correct(self):
+		settings = frappe.get_single("Flexible Hours Settings")
+		self.assertEqual(settings.weekly_target_hours, 54)
+		self.assertEqual(settings.daily_min_hours, 7)
+		self.assertEqual(settings.daily_max_hours, 11)
 
-    def test_can_save_settings(self):
-        settings = frappe.get_single("Flexible Hours Settings")
-        settings.weekly_target_hours = 48
-        settings.save()
-        reloaded = frappe.get_single("Flexible Hours Settings")
-        self.assertEqual(reloaded.weekly_target_hours, 48)
-        # restore
-        settings.weekly_target_hours = 54
-        settings.save()
+	def test_can_save_settings(self):
+		settings = frappe.get_single("Flexible Hours Settings")
+		try:
+			settings.weekly_target_hours = 48
+			settings.save()
+			reloaded = frappe.get_single("Flexible Hours Settings")
+			self.assertEqual(reloaded.weekly_target_hours, 48)
+		finally:
+			settings.weekly_target_hours = 54
+			settings.save()
